23409caa6293c3b18881f300a83a613cf837cfd6
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-14 17:14:31 +0100

    console

---
+++ JS/main.js
@@ -0,0 +1,52 @@
+// Data una lista della spesa, 
+// stampare sulla pagina gli elementi della lista individualmente con un ciclo for che poi commentate, 
+// e poi fate lo stesso con il while (che lasciate attivo).
+
+// dichiarazione arr
+let arr = ["latte", "miele", "sale", "pepe", "sale", "sale"];
+
+// ciclo for console esplicativo 
+// for(let i = 0; i < arr.length; i++){
+//     console.log("arr[" + i + "]: " + arr[i]);
+
+// };
+
+// ciclo while
+let i = 0;
+while(i < arr.length){
+    console.log("arr[" + i + "]: " + arr[i]);
+
+    
+    i++;
+};
+
+
+
+
+
+
+// ciclo for console esplicativo 
+// for(let i = 0; i < arr.length; i++){
+//     console.log("arr[" + i + "]: " + arr[i]);
+
+// };
+
+// ciclo while console esplicativo 
+// let i = 0;
+// while(i < arr.length){
+//     console.log("arr[" + i + "]: " + arr[i]);
+
+//     i++;
+// };
+
+// for(){};
+
+// A = document.getElementById("B").value;
+
+
+
+
+// console.log("");
+// for(let i = 0; i < 10; i++){
+
+// }
